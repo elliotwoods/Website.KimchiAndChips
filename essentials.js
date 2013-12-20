c91e7d915eb538fc3ba40ba767bd420063841332
Elliot Woods <elliot@kimchiandchips.com>
2013-12-20 20:27:30 +0900

    working on about, something broke

---
+++ essentials.js
@@ -0,0 +1,14 @@
+window.onscroll = function(args) {
+    var y = window.pageYOffset;
+    var navBar = document.getElementById("navigationBar");
+    var newY = 0;
+    if (y < 0) {
+        newY = 66;
+    } else if (y < 66) {
+        newY = 66 - y;
+    } else {
+        newY = 0;
+    }
+    newY += 'px'
+    navBar.style.top = newY;
+}
