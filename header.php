b7db0e3f9e87ba2b941c816733b79df7ade97206
Elliot Woods <elliot@kimchiandchips.com>
2014-03-31 18:20:37 +0200

    add google analytics

--- header.php
+++ header.php
@@ -8,6 +8,16 @@
 		<script type="text/javascript" src="javascript/underscore-min.js"></script>
 		<script type="text/javascript" src="essentials.js"></script>
 		<script type="text/javascript">
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-11876799-1', 'kimchiandchips.com');
+		  ga('send', 'pageview');
+		
+		</script>
 		var isLive = <?= $GLOBALS['isLive'] ? "true" : "false"; ?>;
 		</script>
 		
