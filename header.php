b065e6a0ebce5711ee857affc788615b622b2e7f
Elliot Woods <elliot@kimchiandchips.com>
2014-04-08 15:00:07 +0900

    header fix

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
 		<script type="text/javascript" src="javascript/jquery.ba-hashchange.min.js"></script>
 		<script type="text/javascript" src="javascript/underscore-min.js"></script>
 		<script type="text/javascript" src="essentials.js"></script>
-		<script type="text/javascript">
 		<script>
 		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
