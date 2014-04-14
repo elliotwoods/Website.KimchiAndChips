49aed611bcf6ef983949155946a65852d30481e2
Elliot Woods <elliot@kimchiandchips.com>
2014-04-14 19:22:43 +0900

    go back to old tracking code

--- header.php
+++ header.php
@@ -7,14 +7,19 @@
 		<script type="text/javascript" src="javascript/jquery.ba-hashchange.min.js"></script>
 		<script type="text/javascript" src="javascript/underscore-min.js"></script>
 		<script type="text/javascript" src="essentials.js"></script>
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-		  ga('create', 'UA-11876799-1', 'kimchiandchips.com');
-		  ga('send', 'pageview');
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-11876799-1]);
+			_gaq.push(['_setDomainName', 'kimchiandchips.com']);
+			_gaq.push(['_setAllowLinker', true]);
+			_gaq.push(['_trackPageview']);
+			_gaq.push(['_setAllowAnchor', true]); 
+			
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		</script>
 		<script type="text/javascript">
 			var isLive = <?= $GLOBALS['isLive'] ? "true" : "false"; ?>;
