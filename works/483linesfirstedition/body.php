7dbec89a5733993529458a338ffa5d0bedf86270
Yoona Yang <yoona@kimchiandchips.com>
2019-04-16 10:52:49 +0900

    change text for 483 lines first edition

--- works/483linesfirstedition/body.php
+++ works/483linesfirstedition/body.php
@@ -18,13 +18,14 @@
 	<div class="padding-80"></div>
 
 	<div class="works_02 text_en">
-        Kimchi and Chips' previous work Line Segments Space fills the gallery with a web of dynamic imagery. 
-        At the end of last year, the Jeju Museum of Art commissioned a new version of 
-        <span class="press-link"><a href="/works/linesegmentsspace/">
-		「Line Segments Space, 2013」</a></span> for the exhibition From Moment To Moment. 
-		The museum designed by Korean architect Mijung Kim is a panorama of reflective floors and square concrete apertures, and these architectural characteristics demanded a reconsidering of the work and the principals by which it is arranged, leading the artists into a different path and ultimately creating a new work, 483 lines.
+		<?= render_markdown_file("description_en.md") ?>
 	</div>
 
+	<div class="padding-60"></div>
+
+	<div class="works_02 text_kr" style="color: #000000;">
+		<?= render_markdown_file("description_kr.md") ?>
+	</div>
 
 	<div class="padding-60"></div>
 
@@ -37,13 +38,6 @@
 		<script src="https://player.vimeo.com/api/player.js"></script>
 	</div>
 
-	<div class="padding-60"></div>
-
-	<div class="works_02 text_en">
-		From World War II up until the recent end of analogue broadcasts, decades of living imagery had been constructed using the NTSC standard. 
-		This standard represents a moving image frame as 483 lines of modulated light stacked from the top to the bottom of a television screen, within each line there is an analogue continuum, like the groove on a record player. 
-		From Nam Jun Paik to the moon landings, pictures were being represented, archived and seen within this format, until the line made way for the pixel and the digital video revolution.
-	</div>
 
 	<div class="padding-60"></div>
 
@@ -51,19 +45,10 @@
 		<?= render_image("installation_01.jpg", 'alt="01" style="width:100%"') ?>
 	</div>
 
-	<div class="padding-60"></div>
-
-	<div class="works_02 text_en">
-		The artwork 483 lines magnifies this analogue video picture until it is 16 meters wide, and then folds this image several times so that it fits vertically into the gallery space, therein adding oscillations of depth into the image which can be activated by 'tuning' the projected video to match these waves. 
-		At this scale, each line of video can be individually inspected as its own agent beyond its contribution to the total image. 
-		This follows a common motif within the artists work, to create 2 scales of experience, this time the beating panoramic imagery contrasts the delicate physicality of the fine thread elements.
-        <p></p>
-        The strictly arranged lines can be illusionary, creating a confusing architecture of horizons, whilst the video played through it displays a parallel past, present and future.
-	</div>
-
 
 	<div class="padding-60"></div>
 
+	
 	<div class="works_03">
 		<?= render_image("installation_02.jpg", 'alt="02" style="width:100%"') ?>
 	</div>
