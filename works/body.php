5e28834b70db70cd6682cd6495904c56cafaa623
Yoona Yang <yoona@kimchiandchips.com>
2019-10-20 18:36:07 +0900

    test

--- works/body.php
+++ works/body.php
@@ -30,68 +30,81 @@
         <?= render_image("/works/lightbarrierthirdedition/head.jpg", 'alt="Light Barrier Third Edition" style="width:100%" class="image"') ?>
         <div class="main-name"> LIGHT BARRIER <br> THIRD EDITION <p></p>
         2016</div>
-    </a> 
-    
-     <a href="/works/483linessecondedition/">
-        <?= render_image("/works/483linessecondedition/main.jpg", 'alt="483 Lines Second Edition"  style="width:100%" class="image"') ?>
-        <div class="main-name"> 483 LINES <br> SECOND EDITION <p></p>
-        2015</div>
-    </a> 
-    
+    </a>     
 </div>
 
 
 <div class="padding-80"></div>
 
-
-
 <div class="threeCol">
-
     <div class="leftwork-main">
-        <a href="/works/obscurities/"> 
+        <a href="/works/483linessecondedition/">
+         <?= render_image("/works/483linessecondedition/main.jpg", 'alt="483 Lines Second Edition"  style="width:100%" class="image"') ?>
+            <div class="main-name"> 483 LINES <br> SECOND EDITION <p></p>
+            2015</div>
+        </a> 
+    </div>
+    <div class="centerwork-main">
+         <a href="/works/obscurities/"> 
             <?= render_image("/works/obscurities/head.jpg", 'alt="Obscurities"  style="width:100%" class="image"') ?>
             <div class="main-name"> OBSCURITIES  <p></p>
             2017</div>
-        </a>
+        </a>  
     </div>
-    <div class="centerwork-main">
+    <div class="rightwork-main">
         <a href="/works/lightbarriersecondedition/"> 
             <?= render_image("/works/lightbarriersecondedition/main.jpg", 'alt="Light Barrier Second Edition"  style="width:100%" class="image"') ?>
             <div class="main-name"> LIGHT BARRIER <br> SECOND EDITION  <p></p>
             2015</div>
-        </a>    
+        </a>  
     </div>
-    <div class="rightwork-main">
+</div>
+
+<div class="threeCol">
+    <div class="leftwork-main">
         <a href="/works/lunarsurface/"> 
             <?= render_image("/works/lunarsurface/main.jpg", 'alt="Lunar Surface" style="width:100%" class="image"') ?>
             <div class="main-name"> LUNAR SURFACE  <p></p>
             2014, 2015</div>
-        </a>        
+        </a>   
     </div>
-
+    <div class="centerwork-main">
+        <a href="/works/483linesfirstedition/">
+            <?= render_image("/works/483linesfirstedition/483lines1_edit.jpg", 'alt="483 Lines First Edition" style="width:100%" class="image"') ?>
+            <div class="main-name"> 483 LINES <br> FIRST EDITION  <p></p>
+            2014</div>
+        </a>   
+    </div>
+    <div class="rightwork-main">
+        <a href="/works/lightbarrier/">
+            <?= render_image("/works/lightbarrier/lightbarrier1_edit.jpg", 'alt="Light Barrier" style="width:100%" class="image"') ?> 
+            <div class="main-name"> LIGHT BARRIER  <p></p>
+            2014</div>
+        </a>         
+    </div>
 </div>
 
 
 <div class="threeCol">  
     <div class="leftwork-main">
-        <a href="/works/483linesfirstedition/">
-            <?= render_image("/works/483linesfirstedition/483lines1_edit.jpg", 'alt="483 Lines First Edition" style="width:100%" class="image"') ?>
-            <div class="main-name"> 483 LINES <br> FIRST EDITION  <p></p>
-            2014</div>
-        </a>
-    </div>
-    <div class="centerwork-main">
-        <a href="/works/lightbarrier/">
-            <?= render_image("/works/lightbarrier/lightbarrier1_edit.jpg", 'alt="Light Barrier" style="width:100%" class="image"') ?> 
-            <div class="main-name"> LIGHT BARRIER  <p></p>
-            2014</div>
-        </a>    
-    </div>
-    <div class="rightwork-main">
         <a href="/works/linesegmentsspace/">
             <?= render_image("/works/linesegmentsspace/main_edit.jpg", 'alt="Line Segments Space" style="width:100%" class="image"') ?>
             <div class="main-name"> LINE SEGMENTS SPACE  <p></p>
             2013</div>
+        </a>  
+    </div>
+    <div class="centerwork-main">
+        <a href="/works/assembly/">
+            <?= render_image("/works/assembly/main.jpg", 'alt="Assembly" style="width:100%"') ?>
+            <div class="main-name"> ASSEMBLY  <p></p>
+            2012</div>
+        </a> 
+    </div>
+    <div class="rightwork-main">
+        <a href="/works/littree/">
+            <?= render_image("/works/littree/main.jpg", 'alt="Lit Tree" style="width:100%"') ?>
+            <div class="main-name"> LIT TREE  <p></p>
+            2011</div>
         </a>    
     </div>
 </div>  
@@ -99,25 +112,15 @@
 
 <div class="threeCol">  
     <div class="leftwork-main">
-        <a href="/works/assembly/">
-            <?= render_image("/works/assembly/main.jpg", 'alt="Assembly" style="width:100%"') ?>
-            <div class="main-name"> ASSEMBLY  <p></p>
-            2012</div>
-        </a>    
-    </div>
-    <div class="centerwork-main">
-        <a href="/works/littree/">
-            <?= render_image("/works/littree/main.jpg", 'alt="Lit Tree" style="width:100%"') ?>
-            <div class="main-name"> LIT TREE  <p></p>
-            2011</div>
-        </a>    
-    </div>
-    <div class="rightwork-main">
         <a href="/works/link/">
             <?= render_image("/works/link/link1_edit.jpg", 'alt="Link" style="width:100%"') ?>
             <div class="main-name"> LINK  <p></p>
             2010</div>
-        </a>    
+        </a> 
+    </div>
+    <div class="centerwork-main">
+    </div>
+    <div class="rightwork-main">
     </div>
 </div>
 
